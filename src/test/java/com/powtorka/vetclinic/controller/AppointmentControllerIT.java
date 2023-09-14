ace8df848cb216673b8c50f076e1d167316ffa9d
mechu <chudziklmateusz@gmail.com>
2023-09-14 17:26:42 +0200

    added test

--- src/test/java/com/powtorka/vetclinic/controller/AppointmentControllerIT.java
+++ src/test/java/com/powtorka/vetclinic/controller/AppointmentControllerIT.java
@@ -1,14 +1,12 @@
 package com.powtorka.vetclinic.controller;
 
+
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.powtorka.vetclinic.DatabaseCleaner;
 import com.powtorka.vetclinic.VetclinicApplication;
-import com.powtorka.vetclinic.model.appointment.Appointment;
-import com.powtorka.vetclinic.model.appointment.AppointmentDto;
-import com.powtorka.vetclinic.service.AppointmentService;
 import liquibase.exception.LiquibaseException;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,13 +14,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import java.time.LocalDateTime;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -30,39 +35,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class AppointmentControllerIT {
-
     private final MockMvc postman;
     private final ObjectMapper objectMapper;
     private final DatabaseCleaner databaseCleaner;
-    private final AppointmentService appointmentService;
-
-    @Autowired
-    public AppointmentControllerIT(
-            MockMvc mockMvc,
-            ObjectMapper objectMapper,
-            DatabaseCleaner databaseCleaner,
-            AppointmentService appointmentService
-    ) {
-        this.postman = mockMvc;
-        this.objectMapper = objectMapper;
-        this.databaseCleaner = databaseCleaner;
-        this.appointmentService = appointmentService;
-    }
 
     @AfterEach
     void tearDown() throws LiquibaseException {
         databaseCleaner.cleanUp();
     }
 
-    @Test
-    public void shouldFindAppointmentById() throws Exception {
-        postman.perform(get("/appointment/1"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.doctorId").value(1))
-                .andExpect(jsonPath("$.patientId").value(1))
-                .andExpect(jsonPath("$.dateTime").value("2023-08-31T20:26:03.93"))
-                .andExpect(jsonPath("$.price").value(105.2));
+    @Autowired
+    public AppointmentControllerIT(MockMvc postman, ObjectMapper objectMapper, DatabaseCleaner databaseCleaner) {
+        this.postman = postman;
+        this.objectMapper = objectMapper;
+        this.databaseCleaner = databaseCleaner;
     }
 
     @Test
@@ -76,5 +62,18 @@ public class AppointmentControllerIT {
                 .andExpect(jsonPath("$.price").value(50.0));
     }
 
+    @Test
+    public  void  shuldDeleteAppointment() throws Exception {
+        postman.perform(delete("/appointment/2"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Appointment with ID: 2 has been deleted"));
+
+    }
+
+
+
 
 }
+
+
