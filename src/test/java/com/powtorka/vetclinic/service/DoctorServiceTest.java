67bfb94a9d23983d0c04cee47b768885b9863c21
Michał Barnat <michalbarnat@gmail.com>
2023-09-27 10:44:13 +0200

    DoctorServiceTest Mockito tests done

--- src/test/java/com/powtorka/vetclinic/service/DoctorServiceTest.java
+++ src/test/java/com/powtorka/vetclinic/service/DoctorServiceTest.java
@@ -1,6 +1,8 @@
 package com.powtorka.vetclinic.service;
 
+import com.powtorka.vetclinic.exceptions.DoctorNotFoundException;
 import com.powtorka.vetclinic.model.doctor.Doctor;
+import com.powtorka.vetclinic.model.doctor.UpdateDoctorCommand;
 import com.powtorka.vetclinic.repository.DoctorRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,13 +11,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class DoctorServiceTest {
@@ -29,6 +32,19 @@ public class DoctorServiceTest {
     @BeforeEach
     public void init() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testSave() {
+        Doctor doctor = new Doctor();
+        doctor.setName("John");
+
+        when(doctorRepositoryMock.save(any(Doctor.class))).thenReturn(doctor);
+
+        Doctor savedDoctor = doctorServiceMock.save(doctor);
+
+        assertEquals("John", savedDoctor.getName());
+        verify(doctorRepositoryMock).save(doctor);
     }
 
     @Test
@@ -65,6 +81,23 @@ public class DoctorServiceTest {
     }
 
     @Test
+    public void testFindAll2() {
+        Doctor doctor1 = new Doctor();
+        Doctor doctor2 = new Doctor();
+
+        List<Doctor> doctors = Arrays.asList(doctor1, doctor2);
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<Doctor> doctorPage = new PageImpl<>(doctors, pageable, doctors.size());
+
+        when(doctorRepositoryMock.findAll(pageable)).thenReturn(doctorPage);
+
+        Page<Doctor> retrievedDoctors = doctorServiceMock.findAll(pageable);
+
+        assertEquals(2, retrievedDoctors.getTotalElements());
+        verify(doctorRepositoryMock).findAll(pageable);
+    }
+
+    @Test
     public void testDeleteById() {
         Long doctorId = 1L;
         Doctor doctor = new Doctor();
@@ -78,26 +111,100 @@ public class DoctorServiceTest {
         verify(doctorRepositoryMock, times(1)).deleteById(doctorId);
     }
 
-//    @Test
-//    public void testEditPartially() {
-//        Long doctorId = 1L;
-//        UpdateDoctorCommand command = new UpdateDoctorCommand();
-//        command.setName("New Name");
-//        command.setSpeciality("New Speciality");
-//        // Set other fields as needed
-//
-//        Doctor existingDoctor = new Doctor();
-//        existingDoctor.setId(doctorId);
-//        when(doctorRepositoryMock.findById(doctorId)).thenReturn(Optional.of(existingDoctor));
-//        when(doctorRepositoryMock.save(existingDoctor)).thenReturn(existingDoctor);
-//
-//        Doctor editedDoctor = doctorServiceMock.editPartially(doctorId, command);
-//
-//        assertEquals(command.getName(), editedDoctor.getName());
-//        assertEquals(command.getSpeciality(), editedDoctor.getSpeciality());
-//
-//        verify(doctorRepositoryMock, times(1)).findById(doctorId);
-//        verify(doctorRepositoryMock, times(1)).save(existingDoctor);
-//    }
+    @Test
+    public void testDeleteById_ExistingDoctor() {
+        Long doctorId = 1L;
+        when(doctorRepositoryMock.existsById(doctorId)).thenReturn(true);
+
+        doctorServiceMock.deleteById(doctorId);
+
+        verify(doctorRepositoryMock).deleteById(doctorId);
+    }
+
+    @Test
+    public void testDeleteById_NonExistingDoctor() {
+        Long doctorId = 2L;
+        when(doctorRepositoryMock.existsById(doctorId)).thenReturn(false);
+
+        assertThrows(DoctorNotFoundException.class, () -> doctorServiceMock.deleteById(doctorId));
+
+        verify(doctorRepositoryMock, never()).deleteById(doctorId);
+    }
+
+    @Test
+    public void testEditDoctor() {
+        Long doctorId = 1L;
+        Doctor originalDoctor = new Doctor();
+        originalDoctor.setId(doctorId);
+        originalDoctor.setName("Old Name");
+
+        UpdateDoctorCommand command = new UpdateDoctorCommand();
+        command.setName("New Name");
+
+        when(doctorRepositoryMock.findById(doctorId)).thenReturn(Optional.of(originalDoctor));
+
+        Doctor editedDoctor = doctorServiceMock.editDoctor(doctorId, command);
+
+        assertEquals("New Name", editedDoctor.getName());
+        verify(doctorRepositoryMock, times(1)).findById(doctorId);
+    }
+
+    @Test
+    public void testEditPartially_OneField() {
+        Long doctorId = 1L;
+        Doctor originalDoctor = new Doctor();
+        originalDoctor.setId(doctorId);
+        originalDoctor.setName("Old Name");
+        originalDoctor.setSurname("Old Surname");
+
+        UpdateDoctorCommand command = new UpdateDoctorCommand();
+        command.setName("New Name");
+
+        when(doctorRepositoryMock.findById(doctorId)).thenReturn(Optional.of(originalDoctor));
+
+        Doctor editedDoctor = doctorServiceMock.editPartially(doctorId, command);
+
+        assertEquals("New Name", editedDoctor.getName());
+        assertEquals("Old Surname", editedDoctor.getSurname());
+        verify(doctorRepositoryMock, times(1)).findById(doctorId);
+    }
+
+    @Test
+    public void testEditPartially_MultipleFields() {
+        Long doctorId = 1L;
+        Doctor originalDoctor = new Doctor();
+        originalDoctor.setId(doctorId);
+        originalDoctor.setName("Old Name");
+        originalDoctor.setSurname("Old Surname");
+
+        UpdateDoctorCommand command = new UpdateDoctorCommand();
+        command.setName("New Name");
+        command.setSurname("New Surname");
+
+        when(doctorRepositoryMock.findById(doctorId)).thenReturn(Optional.of(originalDoctor));
+
+        Doctor editedDoctor = doctorServiceMock.editPartially(doctorId, command);
+
+        assertEquals("New Name", editedDoctor.getName());
+        assertEquals("New Surname", editedDoctor.getSurname());
+        verify(doctorRepositoryMock, times(1)).findById(doctorId);
+    }
+
+    @Test
+    public void testEditPartially_NoFields() {
+        Long doctorId = 1L;
+        Doctor originalDoctor = new Doctor();
+        originalDoctor.setId(doctorId);
+        originalDoctor.setName("Old Name");
+
+        UpdateDoctorCommand command = new UpdateDoctorCommand();
+
+        when(doctorRepositoryMock.findById(doctorId)).thenReturn(Optional.of(originalDoctor));
+
+        Doctor editedDoctor = doctorServiceMock.editPartially(doctorId, command);
+
+        assertEquals("Old Name", editedDoctor.getName());
+        verify(doctorRepositoryMock, times(1)).findById(doctorId);
+    }
 
 }
