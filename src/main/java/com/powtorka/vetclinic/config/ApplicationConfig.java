6c8d5c3d2d2cafdc0e75b391dd214589962e67c3
Michał Barnat <michalbarnat@gmail.com>
2023-10-12 11:26:07 +0200

    add dependency for swagger springdoc-openapi-starter-webmvc-ui change serverport to 8090

--- src/main/java/com/powtorka/vetclinic/config/ApplicationConfig.java
+++ src/main/java/com/powtorka/vetclinic/config/ApplicationConfig.java
@@ -1,7 +1,11 @@
 package com.powtorka.vetclinic.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
